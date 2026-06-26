9c7636708de7030f0d8147c362b9805785fac982
NeilBarton-NOAA <neil.barton@noaa.gov>
2026-06-26 17:47:49 +0000

    update wcoss2 polling

--- wcoss2_nrt_polling.sh
+++ wcoss2_nrt_polling.sh
@@ -21,7 +21,7 @@ if [[ ${n_files} == 0 ]]; then
 fi
 
 COUNT_FILES
-if (( ${NUM_FILES} >= 3000 )); then
+if (( ${NUM_FILES} != 1605 )); then
     echo "FILES ALREADY PROCESSED FOR" ${dtg}
     exit 0
 fi

b7e4f681a55143af736fac7a92c3c4ae3901ac9c
NeilBarton-NOAA <neil.barton@noaa.gov>
2026-09-01 20:26:13 +0000

    all members for GET_ICS.sh

--- GET_ICS.sh
+++ GET_ICS.sh
@@ -14,7 +14,7 @@ if [[ ${IC_SRC} == "GFS" ]]; then
     files="gdasocean_restart gdasocean_analysis gdas_restarta gdas_restartb enkfgdas \
            enkfgdas_restarta_grp1 enkfgdas_restartb_grp1"
     # comment out below if only need 10 members
-    #files="${files} enkfgdas_restarta_grp2 enkfgdas_restartb_grp2 enkfgdas_restarta_grp3 enkfgdas_restartb_grp3"
+    files="${files} enkfgdas_restarta_grp2 enkfgdas_restartb_grp2 enkfgdas_restarta_grp3 enkfgdas_restartb_grp3"
 elif [[ ${IC_SRC} == "DA_UPDATE" ]]; then
     files="gdasocean_restart gdasocean_analysis gdas_restartb enkfgdas_restarta_grp1 enkfgdas_restartb_grp1"
     # comment out below if only need 10 members
